37289f226b8537af343d005f23ea4eb5b2475e60
fanziwen <fanziwen@xiaomi.com>
2017-09-30 15:33:20 +0800

    can play sound

--- pages/record/record.js
+++ pages/record/record.js
@@ -1,13 +1,21 @@
 //index.js
 //获取应用实例
 const app = getApp()
+let pianoNoteUrls = []
+const PIANOADDRESSSTART=39148
+for (var i = 0 ;i<=87;i++) {
+  pianoNoteUrls.push('https://freesound.org/data/previews/39/'+(PIANOADDRESSSTART+i)+'_35187-lq.mp3')
+}
+const musicBoxNoteUrls = []
 
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    pianoNotes:pianoNoteUrls,
+    activeNote: {}
   },
   //事件处理函数
   bindViewTap: function() {
@@ -15,32 +23,30 @@ Page({
       url: '../logs/logs'
     })
   },
+  touchStartHandler: function(e) {
+    // console.log(e);
+    //http://www.wxapp-union.com/portal.php?mod=view&aid=2830  how to setData
+    var key = 'activeNote'+'.'+e.target.id;
+    var index = +e.target.id.split('note')[1]+51
+    console.log(index)
+    this.audioCtx[index].seek(0);
+    this.audioCtx[index].play();
+    this.setData({
+      [key]:true
+    })
+  },
+  touchEndHandler: function(e) {
+    var key = 'activeNote' + '.' + e.target.id;
+    this.setData({
+      [key]: false
+    })
+  },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+    var self = this;
+    this.audioCtx = []
+    for (let i = 52;i<=64;i++) {
+      this.audioCtx[i] = wx.createAudioContext('piano'+(i+1))
+      this.audioCtx[i].setSrc(pianoNoteUrls[i]) //TODO: 看是模板直接赋值src还是这里赋值
     }
   },
   getUserInfo: function(e) {
